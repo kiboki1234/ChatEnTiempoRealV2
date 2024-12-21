14ed8173e50178d0d40a1e6d6b9415afcab3320d
EspinAndres01 <andresboki2000@gmail.com>
2024-12-21 16:07:59 -0500

    nuevo ico

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,10 @@ import ChatBox from './components/ChatBox';
 const App = () => {
     return (
         <div>
-           
             <ChatBox />
+            <footer style={{ textAlign: 'center', marginTop: '20px', padding: '10px', fontSize: '12px', color: 'gray' }}>
+                &copy; {new Date().getFullYear()} Kibotech. Todos los derechos reservados.
+            </footer>
         </div>
     );
 };
