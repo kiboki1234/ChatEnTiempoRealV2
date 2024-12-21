24cda2635c225592edad0889067d7f8f27e846b4
EspinAndres01 <andresboki2000@gmail.com>
2024-12-21 10:59:16 -0500

    levantandoServicio

--- frontend/src/services/socketService.js
+++ frontend/src/services/socketService.js
@@ -1,6 +1,6 @@
 // src/services/socketService.js
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5000');
+const socket = io('process.env.REACT_APP_SOCKET_SERVER_URL');
 
 export default socket;
